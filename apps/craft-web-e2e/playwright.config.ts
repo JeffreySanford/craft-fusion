61d05e414902f583205e60db93efe712fc678e03
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-12-04 12:31:09 -0600

    Remove unused e2e test files and update TypeScript configurations for improved type declaration support

--- apps/craft-web-e2e/playwright.config.ts
+++ apps/craft-web-e2e/playwright.config.ts
@@ -1,25 +1,18 @@
 import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
-
 import { workspaceRoot } from '@nx/devkit';
-
-// For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { environment } from '../../environments/environment';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   ...nxE2EPreset(__filename, { testDir: './src' }),
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL,
+    baseURL: environment.apiUrl,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -27,7 +20,7 @@ export default defineConfig({
   webServer: {
     command: 'npx nx run craft-web:serve',
     url: 'http://localhost:4200',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env['CI'],
     cwd: workspaceRoot,
   },
   projects: [
@@ -35,17 +28,14 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
     // Uncomment for mobile browsers support
     /* {
       name: 'Mobile Chrome',
@@ -55,7 +45,6 @@ export default defineConfig({
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12'] },
     }, */
-
     // Uncomment for branded browsers
     /* {
       name: 'Microsoft Edge',
