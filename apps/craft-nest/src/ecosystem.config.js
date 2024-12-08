405652104a6bebaf0a07e9b2ea0c8825728ae0e6
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-12-08 16:05:49 -0600

    Rename application to 'craft-nest-api' and update script paths; add logging configuration for production and development environments

--- apps/craft-nest/src/ecosystem.config.js
+++ apps/craft-nest/src/ecosystem.config.js
@@ -1,8 +1,8 @@
 // ecosystem.config.js
 module.exports = {
   apps: [{
-    name: 'craft-nest',
-    script: 'dist/apps/craft-nest/src/main.js',
+    name: 'craft-nest-api',
+    script: 'dist/apps/craft-nest/main.js',
     instances: 'max',
     exec_mode: 'cluster',
     env_production: {
@@ -10,19 +10,21 @@ module.exports = {
       PORT: 443,
       HOST: 'jeffreysanford.us',
       KEY_PATH: '/etc/letsencrypt/live/jeffreysanford.us/privkey.pem',
-      CERT_PATH: '/etc/letsencrypt/live/jeffreysanford.us/fullchain.pem'
+      CERT_PATH: '/etc/letsencrypt/live/jeffreysanford.us/fullchain.pem',
+      error_file: '~/logs/craft-nest/error.log',
+      out_file: '~/logs/craft-nest/out.log'
     },
     env_development: {
       NODE_ENV: 'development',
       PORT: 3000,
       HOST: 'localhost',
-      KEY_PATH: './apps/craft-nest/src/cert/server.key',
-      CERT_PATH: './apps/craft-nest/src/cert/server.crt'
+      KEY_PATH: 'apps/craft-nest/src/cert/server.key',
+      CERT_PATH: 'apps/craft-nest/src/cert/server.crt',
+      error_file: 'logs/error.log',
+      out_file: 'logs/out.log'
     },
     watch: false,
     max_memory_restart: '1G',
-    error_file: '/var/log/craft-nest/error.log',
-    out_file: '/var/log/craft-nest/out.log',
     merge_logs: true,
     time: true
   }]
