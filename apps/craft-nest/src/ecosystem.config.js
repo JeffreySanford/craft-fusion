0292737a2af5b8ddf302fe239a7630c9a357fd2a
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-12-08 16:23:30 -0600

    Update ecosystem configuration to use absolute script paths and consolidate environment variables

--- apps/craft-nest/src/ecosystem.config.js
+++ apps/craft-nest/src/ecosystem.config.js
@@ -1,34 +1,22 @@
 // ecosystem.config.js
 module.exports = {
-  apps: [
-    {
-      name: 'craft-nest-api',
-      script: '../../dist/apps/craft-nest/main.js', // Relative to ecosystem.config.js location
-      instances: 'max',
-      exec_mode: 'cluster',
-      cwd: '../../../', // Relative to ecosystem.config.js location
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 443,
-        HOST: 'jeffreysanford.us',
-        KEY_PATH: '/etc/letsencrypt/live/jeffreysanford.us/privkey.pem',
-        CERT_PATH: '/etc/letsencrypt/live/jeffreysanford.us/fullchain.pem',
-        error_file: '/home/jeffrey/logs/craft-nest/error.log',
-        out_file: '/home/jeffrey/logs/craft-nest/out.log',
-      },
-      env_development: {
-        NODE_ENV: 'development',
-        PORT: 3000,
-        HOST: 'localhost',
-        KEY_PATH: 'apps/craft-nest/src/cert/server.key',
-        CERT_PATH: 'apps/craft-nest/src/cert/server.crt',
-        error_file: 'logs/error.log',
-        out_file: 'logs/out.log',
-      },
-      watch: false,
-      max_memory_restart: '1G',
-      merge_logs: true,
-      time: true,
+  apps: [{
+    name: 'craft-nest-api',
+    script: '/home/jeffrey/repos/craft-fushion/dist/apps/craft-nest/main.js',
+    instances: 'max',
+    exec_mode: 'cluster',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 443,
+      HOST: 'jeffreysanford.us',
+      KEY_PATH: '/etc/letsencrypt/live/jeffreysanford.us/privkey.pem',
+      CERT_PATH: '/etc/letsencrypt/live/jeffreysanford.us/fullchain.pem'
     },
-  ],
+    error_file: '/home/jeffrey/logs/craft-nest/error.log',
+    out_file: '/home/jeffrey/logs/craft-nest/out.log',
+    merge_logs: true,
+    time: true,
+    max_memory_restart: '1G',
+    watch: false
+  }]
 };
