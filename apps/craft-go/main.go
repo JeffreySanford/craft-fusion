c4d1d560990f037f6e8ad8b7c119006dd897e4aa
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-12-23 11:03:30 -0600

    Add CORS support and set trusted proxies in main.go; enable access from localhost and specified domain

--- apps/craft-go/main.go
+++ apps/craft-go/main.go
@@ -11,6 +11,24 @@ import (
 func main() {
 	router := gin.Default()
 
+	// Set trusted proxies
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+
+	//enable CORS from localhost:4200 and jeffreysanford.us
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200, https://jeffreysanford.us")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	// Enable Gzip Compression
 	router.Use(gzip.Gzip(gzip.DefaultCompression))
 
