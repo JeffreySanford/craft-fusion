ebe322c9d04092b65f81793f84874df89882b6b4
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-12-25 12:44:50 -0600

    Update production environment configuration; add nestPort and goPort for service management

--- apps/craft-web/src/environments/environment.prod.ts
+++ apps/craft-web/src/environments/environment.prod.ts
@@ -4,5 +4,7 @@ export const environment = {
   host: 'jeffreysanford.us',
   port: 443,
   keyPath: '/etc/letsencrypt/live/jeffreysanford.us/privkey.pem',
-  certPath: '/etc/letsencrypt/live/jeffreysanford.us/fullchain.pem'
+  certPath: '/etc/letsencrypt/live/jeffreysanford.us/fullchain.pem',
+  nestPort: 3000,
+  goPort: 4000
 };
