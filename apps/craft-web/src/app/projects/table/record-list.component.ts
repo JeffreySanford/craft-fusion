60cbefd7cd2fa4da4e535698abe6e7f0cd7adc91
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-12-28 11:52:19 -0600

    feat(sidebar): remove collapsed state binding and update component structure for improved clarity feat(space-video): remove standalone flag for consistency in component declaration feat(record-detail): remove standalone flag for consistency in component declaration feat(record-list): remove standalone flag for consistency in component declaration style(colors): delete unused color definitions to streamline styles style(theme): enhance theme structure and add support for light and dark themes style(styles): refactor main styles to improve theme management and component styling style(typography): remove unused typography definitions to streamline styles

--- apps/craft-web/src/app/projects/table/record-list.component.ts
+++ apps/craft-web/src/app/projects/table/record-list.component.ts
@@ -16,7 +16,6 @@ export interface Server {
 }
 
 @Component({
-  standalone: false,
   selector: 'app-record-list',
   templateUrl: './record-list.component.html',
   styleUrls: ['./record-list.component.scss'],
