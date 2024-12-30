6887954efdaa9467e171096c30727371e25d667e
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-12-30 08:57:02 -0600

    feat: enhance CI workflow with improved Jest configuration and reporting

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,16 @@
-import { getJestProjectsAsync } from '@nx/jest';
+import nxPreset from './jest.preset';
 
-export default async () => ({
-  rootDir: '.',
-  projects: await getJestProjectsAsync(),
-});
+export default {
+  ...nxPreset,
+  coverageDirectory: './coverage',
+  coverageReporters: ['text-summary', 'lcov'],
+  testEnvironment: 'node',
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './reports/jest',
+      outputName: 'results.xml'
+    }]
+  ],
+  setupFilesAfterEnv: ['./test-setup.ts']
+};
