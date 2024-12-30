c1559763f62e0e291c5bf265a3001cad5c5999a0
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-12-30 15:26:49 -0600

    refactor: update output paths for craft-nest and remove nx-cloud configuration

--- apps/craft-nest/webpack.config.js
+++ apps/craft-nest/webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
+      outputPath: join(__dirname, '../../dist/apps/craft-nest'),
     }),
   ],
 };
