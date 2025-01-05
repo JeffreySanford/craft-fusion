fb0100d7338d89c03c8230b25be86c01da20408d
Jeffrey Sanford <jeffreysanford@gmail.com>
2025-01-05 08:43:37 -0600

    feat: update ecosystem configuration to use relative paths for scripts and log files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,17 +3,17 @@ module.exports = {
   apps: [
     {
       name: 'craft-nest-api',
-      script: 'dist/apps/craft-nest/src/main.js',
+      script: './dist/apps/craft-nest/src/main.js',
       instances: 1,  // Set to a single instance
       exec_mode: 'fork',  // Use fork mode for a single instance
-      cwd: 'C:/repos/craft-fusion',
+      cwd: './',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
         HOST: 'localhost',
       },
-      error_file: 'C:/repos/craft-fusion/logs/craft-nest/error.log',
-      out_file: 'C:/repos/craft-fusion/logs/craft-nest/out.log',
+      error_file: './logs/craft-nest/error.log',
+      out_file: './logs/craft-nest/out.log',
       merge_logs: true,
       time: true,
       max_memory_restart: '1G',
@@ -22,17 +22,17 @@ module.exports = {
     },
     {
       name: 'craft-go-api',
-      script: 'apps/craft-go/dist/apps/craft-go/main',
+      script: './apps/craft-go/dist/apps/craft-go/main',
       instances: 1,
       exec_mode: 'fork',
-      cwd: 'C:/repos/craft-fusion',
+      cwd: './',
       env: {
         NODE_ENV: 'production',
         PORT: 4000,
         HOST: 'localhost',
       },
-      error_file: 'C:/repos/craft-fusion/logs/craft-go/error.log',
-      out_file: 'C:/repos/craft-fusion/logs/craft-go/out.log',
+      error_file: './logs/craft-go/error.log',
+      out_file: './logs/craft-go/out.log',
       merge_logs: true,
       time: true,
       max_memory_restart: '1G',
