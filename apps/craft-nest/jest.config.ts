19528a506f203609e75518d26767b6001410518b
Jeffrey Sanford <jeffreysanford@gmail.com>
2025-01-06 15:33:09 -0600

    refactor: remove unused Jest configuration and clean up TypeScript settings

--- apps/craft-nest/jest.config.ts
+++ apps/craft-nest/jest.config.ts
@@ -1,13 +1,1 @@
-export default {
-  displayName: 'craft-nest',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    "@craft-nest/environments/environment": "<rootDir>/src/environments/environment"
-  },
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/craft-nest',
-};
+// File removed
