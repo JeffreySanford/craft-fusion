533a7fc04add8ce38c29ff6cdb3668f3af29878e
Jeffrey Sanford <jeffreysanford@gmail.com>
2025-01-24 13:51:10 -0600

    feat: add scrollbar styling and enhance table wrapper for improved usability

--- apps/craft-web/src/environments/environment.prod.ts
+++ apps/craft-web/src/environments/environment.prod.ts
@@ -1,5 +1,30 @@
 export const environment = {
   production: true,
   apiUrl: 'https://jeffreysanford.us/api',
-  host: 'jeffreysanford.us'
+  host: 'jeffreysanford.us',
+  finnhubApiKey: 'cu8gbkpr01qt63vgue4gcu8gbkpr01qt63vgue50',
+  finnHubAPI: 'https://finnhub.io/api/v1',
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiamVmZnJleXNhbmZvcmQiLCJhIjoiY201c2psaW8yMG1vMDJrcTJ4ZzNic3YxbyJ9.7e5Pub4Ub0v-tHK9uzIuEA',
+  },
+  nasaFirms: {
+    apiKey: 'your-development-nasa-firms-api-key-here',
+    endpoint: 'https://firms.modaps.eosdis.nasa.gov/api/active-fires',
+  },
+  calFire: {
+    endpoint: 'https://www.fire.ca.gov/api',
+  },
+  flightRadar24: {
+    apiKey: '9df1bdd8-b573-4e99-9b23-ff47fab4c035|PxsBdnqI1p0vbt3PiAi079zIDoq8TK01PTi3ZITYd19a63d7',
+    endpoint: 'https://fr24api.flightradar24.com/api/sandbox',
+  },
+  flightAware: {
+    apiKey: 'nfLsA7DYMbptvaOF4KIkCkSM79WIXArZ', // Ensure this is the correct API key
+    endpoint: 'https://aeroapi.flightaware.com/aeroapi', // Ensure this is the correct endpoint
+  },
+  alphaVantageApiKey: '3EP39QPLULHFVD7Y',
+  yahooFinance: {
+    url: 'https://yfapi.net/',
+    apiKey: 'uANiNS5imM7YUDOxZNpu3qrmLEYkoqu2r6OzqCW8' // Replace with your Yahoo Finance API key
+  }
 };
